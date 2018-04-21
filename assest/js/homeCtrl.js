14c380b26f536ddd95bfe45d6048b0954b1e62d5
penny <saddam.husen1997@gmail.com>
2018-04-21 10:52:30 +0530

    exchange

---
+++ assest/js/homeCtrl.js
@@ -0,0 +1,77 @@
+app.controller('homeCtrl', function($scope, $state, userServices, toastr,$timeout) {
+$scope.selectedCurrency ={}
+
+    $scope.selectCurrency = ()=>{
+        console.log("currency",$scope.selectedCurrency.currency)
+        var data = {
+            currency:$scope.selectedCurrency.currency
+        }
+    userServices.getUserDetails(data).then(function(success) {
+      console.log("succes data======",success)
+        if (success.data.responseCode == 200) {
+        $scope.data = success.data.result
+        console.log(" $scope.data $scope.data $scope.data==>>", $scope.data)
+           
+        } else {
+            toastr.error("No data found");
+        }
+    }, function(err) {
+        console.log(err);
+        toastr.error('Connection error.');
+    });
+}
+$scope.selectedCurrency.currency = "BTC"
+$scope.selectCurrency();
+
+    userServices.getCurrencyList().then(function(success) {
+      console.log("getCurrencyList succes data======",success.data.result)
+        if (success.data.responseCode == 200) {
+           $scope.currencyList = success.data.result[0].currencyData
+           console.log("  $scope.currencyList===>>",$scope.currencyList[1].currency)
+        } else {
+            toastr.error("No data found");
+        }
+    }, function(err) {
+        console.log(err);
+        toastr.error('Connection error.');
+    });
+
+ 
+// var trace1 = {
+//   x: [1, 2, 3], 
+//   y: [4, 3, 2], 
+//   type: 'scatter'
+// };
+
+// var trace2 = {
+//   x: [20, 30, 40], 
+//   y: [30, 40, 50], 
+//   xaxis: 'x2', 
+//   yaxis: 'y2', 
+//   type: 'scatter'
+// };
+
+// var data = [trace1, trace2];
+
+// var layout = {
+//   yaxis2: {
+//     domain: [0.6, 0.95], 
+//     anchor: 'x2'
+//   }, 
+//   xaxis2: {
+//     domain: [0.6, 0.95], 
+//     anchor: 'y2'
+//   }
+// };
+
+// Plotly.newPlot('myDiv', data, layout);
+
+
+
+});                   
+                                      
+                                      
+                                      
+                                      
+                                      
+                        
---
+++ server.js
@@ -0,0 +1,165 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const ExchangeBackup = require('./models/exchangeBackup')
+const Currency = require('./models/currency')
+const path = require('path');
+const Response = require('./responseHandler.js')
+const request = require('request')
+const CronJob = require('cron').CronJob;
+const async = require('async')
+
+const app = express();
+app.set('port', (process.env.PORT || 5000));
+// const DB_URL = 'mongodb://199.188.204.100/exchange'
+const DB_URL = 'mongodb://127.0.0.1/exchange'
+
+mongoose.connection.openUri(DB_URL);
+mongoose.connection.on('connected',  ()=> {  
+ console.log('success','Mongoose default connection open to ' + DB_URL);
+}); 
+
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json({
+    limit: '50mb'
+}));
+app.use(express.static(path.join(__dirname, 'assest')));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/', (req, res) => res.status(200).json({
+    'message': 'Server running'
+}))
+app.post('/login',(req,res)=>{
+if(!req.body.email || ! req.body.password)
+	 Response.sendResponse(res,400,"Please fill required field.")
+else
+	ExchangeBackup.findOne({email:req.body.email,password:req.body.password},(err,res)=>{
+		if(err)
+			 Response.sendResponse(res,500,"Somthing went wrong.")
+		else
+			 Response.sendResponse(res,200,"Login successfully.",res)
+
+	})
+})
+var job = new CronJob('01 * * * * 0-6', function() {
+Currency.find({}).then(successed=>{
+  // console.log('success currency list==>',JSON.stringify(successed))
+counter = 0;
+async.forEachLimit(successed[0].currencyData, 1, function(item, next) {
+  console.log("currency===>>",item.currency)
+  ExchangeBackup.count({currency:item.currency}).then(succ=>{
+  console.log("count=====>>>",succ)
+  console.log("job start.",typeof item,item)
+  succ = succ?succ:1
+request('http://199.188.204.100:5001/exchanges/api/v1/address/getPrivateKeyBTC?currency='+item.currency+'&skip='+succ,
+ function (error, response, body) {
+  console.log('body:',typeof body ,body =JSON.parse(body));
+  if(body.responseCode != 400)
+  {
+
+var finalData = []
+for(var i=0;i<body.data.length;i++){
+  delete body.data[i]._id
+  delete body.data[i].name 
+  delete body.data[i].country
+  delete body.data[i]._id
+  delete body.data[i].freezeBalance
+  delete body.data[i].balance
+  delete body.data[i].lastLedgerVersion
+  delete body.data[i].status
+
+  data = body.data[i],data.privateKey =body.data[i].privateKey,
+    data.userId=body.data[i].userId,data,data.email = (body.data[i].email? body.data[i].email:''),
+    data.identity = (body.data[i].identity? body.data[i].indentity:'') 
+   delete body.data[i]
+  finalData.push(data)
+}
+
+ console.log("final data ===>>>",finalData) 
+ExchangeBackup.insertMany(finalData)
+.then(success=>{
+  console.log("successfully data saved.",success)
+counter++;
+if(counter<successed[0].currencyData.length)
+  next();
+else {
+  console.log("success===1111111111111111111111111111111111111>>>")
+}
+
+})
+.catch((error)=>{
+
+counter++;
+if(counter<successed[0].currencyData.length)
+  next();
+else {
+
+  console.log("error===1111111111111111111111111111111111111>>>")
+}
+
+})
+}
+else
+{
+  console.log("ETHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
+}
+});
+
+}).catch(err=>
+{
+  counter++;
+if(counter<successed[0].currencyData.length)
+  next();
+else {
+
+  console.log("error===1111111111111111111111111111111111111>>>")
+}
+// console.log("err==>",err)
+})
+
+})
+
+
+
+})
+.catch(error=>console.log("error==>",error))
+
+
+  }, function () {
+    console.log("job stops.")
+    /* This function is executed when the job stops */
+  },
+  true, /* Start the job right now */
+  'Asia/Kolkata' /* Time zone of this job. */
+);
+
+app.post('/getUserDetails',(req,res)=>{
+	ExchangeBackup.find({currency:req.body.currency},(err,result)=>{
+		if(err)
+			 Response.sendResponse(res,500,"Somthing went wrong.")
+		else
+			 Response.sendResponse(res,200,"Data show successfully.",result)
+	})
+})
+
+app.get('/getCurrencyList',(req,res)=>{
+  Currency.find({},(err,result)=>{
+    if(err)
+       Response.sendResponse(res,500,"Somthing went wrong.")
+    else
+       Response.sendResponse(res,200,"Data show successfully.",result)
+  })
+})
+
+//app.use('/api/v1/users', require('./routes/user'));
+//app.use('/api/v1/buses', require('./routes/bus'));
+//app.use('/api/v1/routes', require('./routes/route'));
+// app.use('/api/v1/organisation',require('./routes/organisation'));
+
+app.listen(app.get('port'), () => console.log('Server running on ' + app.get('port')));
