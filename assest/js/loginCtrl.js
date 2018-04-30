9c1e5655885c224f959560453f135ca51dcfdc87
utkarsh <utkarshvashishtha123@gmail.com>
2018-04-30 16:35:46 +0530

    superadmin login

--- assest/js/loginCtrl.js
+++ assest/js/loginCtrl.js
@@ -12,12 +12,17 @@ $scope.login = ()=> {
   $scope.data.location = "sector-49, gurgaon"
     userServices.login($scope.data).then(function(success) {
         console.log("success",success.data)
-      if(success.data.responseCode == 200){   
+      if(success.data.responseCode == 200){ 
+      if(success.data.data.role != 'superadmin')
+        toastr.error("Wrong credentials.");
+      else
+      {
           toastr.success(success.data.responseMessage);
          localStorage.setItem('_id',success.data.data._id)
          $state.go('home');
+       }
       }else{
-         toastr.error("Connection error.");
+         toastr.error(success.data.responseMessage);
       }     
      },function(err){
         console.log(err);
